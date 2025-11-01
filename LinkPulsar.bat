5f36dc849d6c16d3f62d2ef99df62cbde1473a1d
Viktor Ferenczi <viktor@ferenczi.eu>
2025-11-01 10:40:45 +0100

    Clarification

--- LinkPulsar.bat
+++ LinkPulsar.bat
@@ -1,2 +1,4 @@
 REM Location of Pulsar (because Rider does not support %AppData% in run configs)
+REM This is required only by the Pulsar run config for Rider. If you are using VS, 
+REM then you don't need to run this script at all.
 mklink /J Pulsar "%AppData%\Pulsar"
